f5b11aaea63fc6205ab1bdb6c1cb07f8c5316a3b
Abady <gamersspeaks@gmail.com>
2018-08-09 00:51:57 +0300

    better now!

--- bot.js
+++ bot.js
@@ -322,10 +322,6 @@ else if(args[0].toLowerCase().startsWith("wank")) {
 nsfwimg = await neko.getNSFWPussyWankGif()
 message.channel.send(randomResponds, {files: [nsfwimg.url]}).catch(err => errormsg(message, err, "nsfw"))
 }
-else if(args[0].toLowerCase().startsWith("blowjob")) {
-nsfwimg = await neko.getNSFWBlowJob()
-message.channel.send(randomResponds, {files: [nsfwimg.url]}).catch(err => errormsg(message, err, "nsfw"))
-}
 }
 
 //////////////////////////
@@ -457,11 +453,11 @@ else if(message.content.startsWith(`${prefix}mute`)){
   }
   else
   if (message.content.startsWith(`${prefix}clear`)) {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: You don't have permissions to clear messages.");
-  if(!args[0]) return message.channel.send(":x: Please specify the number of messages to clear!");
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`<:megThumbs:475427359898599441> Cleared **${args[0]}** messages.`).then(msg => msg.delete(2000));
-  })
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: You don't have permissions to clear messages.");
+  if(!args[0] || !isNaN(args)) return message.channel.send(":x: Please specify the number of messages to clear!");
+  const messages = message.channel.fetchMessages({limit: args[0]});
+  await message.channel.bulkDelete(messages.size)
+  message.channel.send(`<:megThumbs:475427359898599441> Cleared **${message.size}** messages.`).then(msg => msg.delete(3000));
 }
 ///////////////////////////////PREMIUM////////////////////////////////////
 if(client.user.id === premium1.id && message.author.id === premium1.owner) {
