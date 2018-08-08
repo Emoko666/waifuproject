d75f42a8c83e999dc6ee352ba911e46b37c8da04
ProSDLMFDQFK <businessblueox@gmail.com>
2018-08-08 17:15:58 +0200

    Updated

--- bot.js
+++ bot.js
@@ -459,7 +459,7 @@ else if(message.content.startsWith(`${prefix}mute`)){
   else
   if (message.content.startsWith(`${prefix}clear`)) {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Eh,You Don't Have Premission yo do that");
-  if(!args[0]) return message.channel.send("Please Provide a number to clear");
+  if(!args[0]) return message.channel.send(":x: Please specify the number of messages to clear!");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Cleared ${args[0]} messages.<:megThumbs:475427359898599441>`).then(msg => msg.delete(2000));
   })
